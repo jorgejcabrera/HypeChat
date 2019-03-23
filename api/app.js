926292e188bc322a6f0abfef666ad8e2cdb0d77b
Rodri Zapico <rodri.zapico@gmail.com>
2019-03-23 18:30:07 -0300

    added server-wide CORS support

--- api/app.js
+++ api/app.js
@@ -8,6 +8,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Enable CORS.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 require('./routes')(app);
 
 module.exports = app;
