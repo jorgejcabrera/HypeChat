0c4e2de18689686dbd1bc0ad04982825cba5a40c
Rodri Zapico <rodri.zapico@gmail.com>
2019-03-23 20:55:20 -0300

    fixed code style

--- api/app.js
+++ api/app.js
@@ -10,8 +10,11 @@ app.use(express.urlencoded({ extended: false }));
 
 // Enable CORS.
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
