b1333954edbb071bf75e8892508a33e48ece187c
jorgejcabrera <cabrerajjorge@gmail.com>
2019-04-13 23:06:38 -0300

    create message recipient table

---
+++ api/src/config/sequelize/migrations/010-create-message-recipient.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('MessageRecipient', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      recipientId: {
+        allowNull: true,
+        type: Sequelize.INTEGER,
+        references: { model: 'User', key: 'id' },
+      },
+      recipientGroupId: {
+        allowNull: true,
+        type: Sequelize.INTEGER,
+        references: { model: 'UserGroup', key: 'id' },
+      },
+      messageId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Message', key: 'id' },
+      },
+      isRead: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('MessageRecipient');
+  },
+};
