384c2cfce400e66596d711b8658f2911b8697427
Rodri Zapico <rodri.zapico@gmail.com>
2019-04-19 02:14:15 -0300

    fixed last migration

--- api/src/config/sequelize/migrations/011-create-workspace-user.js
+++ api/src/config/sequelize/migrations/011-create-workspace-user.js
@@ -1,43 +1,43 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('WorkspaceUsers', {
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'User',
-          key: 'id',
+  up: async(queryInterface, Sequelize) => {
+    return [
+      await queryInterface.createTable('WorkspaceUsers', {
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'User',
+            key: 'id',
+          },
         },
-      },
-      workspaceId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Workspace',
-          key: 'id',
+        workspaceId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Workspace',
+            key: 'id',
+          },
         },
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    }).then(() => {
-      queryInterface.addIndex('WorkspaceUsers', ['userId']);
-
-      queryInterface.addIndex('WorkspaceUsers', ['workspaceId']);
-
-      queryInterface.addConstraint('WorkspaceUsers',
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      }),
+      await queryInterface.addIndex('WorkspaceUsers', ['userId']),
+      await queryInterface.addIndex('WorkspaceUsers', ['workspaceId']),
+      await queryInterface.addConstraint('WorkspaceUsers',
         ['userId', 'workspaceId'], {
           type: 'unique',
           name: 'workspace_user_unique_constraint',
-        });
-    });
+        }
+      ),
+    ];
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('WorkspaceUsers');
