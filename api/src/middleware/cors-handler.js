5a8c1c2351845333f75610f5fe8726bdaf65e482
Rodri Zapico <rodri.zapico@gmail.com>
2019-05-29 19:13:41 -0300

    added DELETE to allowed CORS methods

--- api/src/middleware/cors-handler.js
+++ api/src/middleware/cors-handler.js
@@ -7,7 +7,7 @@ CorsHandler.name = 'CorsHandler';
 CorsHandler.cors = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, X-Auth, X-Client'
