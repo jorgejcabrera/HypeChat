dd114f9f5e30a86899864bf00d264478bd8b7b45
jorgejcabrera <cabrerajjorge@gmail.com>
2019-06-22 19:03:43 -0300

    add email unique constraint

---
+++ api/src/config/sequelize/migrations/022-add-unique-email-constraint.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addConstraint('User', ['email'], {
+        type: 'unique',
+        name: 'user_email_unique_constraint',
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeConstraint('User', 'user_email_unique_constraint');
+  },
+};
